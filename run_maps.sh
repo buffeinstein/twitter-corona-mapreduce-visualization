6e4f7b33d6ad343a0ec1e99031d2ad5d4c7083c1
buffeinstein <ambikatiwari999@gmail.com>
2025-02-20 14:03:08 -0800

    remove previously created reduced files

--- run_maps.sh
+++ run_maps.sh
@@ -1,7 +1,6 @@
 #map.py works on a single zip file = 1 day 
 #this script will feed in all the zip files from 2019 to map.py 
 
-for file in /data/Twitter\ dataset/geoTwitter19-01-0*.zip; do
+for file in /data/Twitter\ dataset/geoTwitter19*.zip; do
     ./src/map.py --input_path="$file" &
 done 
-
--- src/map.py
+++ src/map.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3:
+#!/usr/bin/env python
 
 #SOME CONTEXT 
 #server = YEAR = 2020
